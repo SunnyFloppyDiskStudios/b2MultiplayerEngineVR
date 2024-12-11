7e7a6348a90d1cbdb726fa46e983d7c6f518ece6
SunnyFlops <arav.prasad@outlook.com>
2024-12-11 15:45:31 +1300

    started working on unlinking client

---
+++ Assets/Scripts/UnlinkClient.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class UnlinkClient : MonoBehaviour {
+    public bool linkToServer = false;
+
+    private KeyCode unlinkKey = KeyCode.JoystickButton6;
+    
+    private void Start() {
+        linkToServer = true;
+    }
+
+    private void Update() {
+        
+    }
+
+    public void Unlink() {
+        
+    }
+    
+}
